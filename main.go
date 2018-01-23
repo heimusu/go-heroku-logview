d5bff706c7c423e7d7d9470574718fa3fa060ec0
久保陽一郎 <kubo_yoichiro@applibot.co.jp>
2018-01-23 14:54:57 +0900

    add echo

--- main.go
+++ main.go
@@ -1,22 +1,53 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+// 	"os"
+// )
+
+// func main() {
+// 	port := os.Getenv("PORT")
+// 	if port == "" {
+// 		log.Fatal("$PORT must be set")
+// 	}
+
+// 	http.HandleFunc("/", handler)
+// 	http.ListenAndServe(":"+port, nil)
+// }
+
+// func handler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Hello, %q", r.URL.Path[1:])
+// }
+
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
 	"os"
+	"log"
+	"./handler"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
+	// Echoのインスタンス作る
+	e := echo.New()
 	port := os.Getenv("PORT")
 	if port == "" {
-		log.Fatal("$PORT must be set")
+		// log.Fatal("$PORT must be set")
+		port = ":1323"
 	}
+	
+	// 全てのリクエストで差し込みたいミドルウェア（ログとか）はここ
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":"+port, nil)
-}
+	// ルーティング
+	e.GET("/hello", handler.MainPage())
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", r.URL.Path[1:])
+	// サーバー起動
+	// e.Start(":1323") //ポート番号指定してね
+	e.Start(port)
 }
